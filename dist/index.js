b1e22385415e3cdd761513290d367cbeff234f1d
Poh Chi Jun <cpoh@moneylion.com>
2024-05-20 13:53:47 +0800

    Debug

--- dist/index.js
+++ dist/index.js
@@ -1863,8 +1863,7 @@ function run() {
         const argocd = yield setupArgoCDCommand();
         const apps = yield getApps();
         core.info(`Found apps: ${apps.map(a => a.metadata.name).join(', ')}`);
-        const output = yield execCommand('pwd');
-        core.info(output.stdout);
+        core.info(__dirname);
         const diffs = [];
         yield asyncForEach(apps, (app) => __awaiter(this, void 0, void 0, function* () {
             const command = `app diff ${app.metadata.name} --server-side-generate --local=${app.spec.source.path}`;
