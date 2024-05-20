8619768af9fc2090ef82a371555323262b0cac99
Poh Chi Jun <cpoh@moneylion.com>
2024-05-20 12:09:49 +0800

    Build

--- dist/index.js
+++ dist/index.js
@@ -1691,6 +1691,7 @@ const github = __importStar(__webpack_require__(469));
 const fs = __importStar(__webpack_require__(747));
 const path = __importStar(__webpack_require__(622));
 const node_fetch_1 = __importDefault(__webpack_require__(454));
+const https_1 = __importDefault(__webpack_require__(211));
 const ARCH = process.env.ARCH || 'linux';
 const githubToken = core.getInput('github-token');
 core.info(githubToken);
@@ -1698,7 +1699,7 @@ const ARGOCD_SERVER_URL = core.getInput('argocd-server-url');
 const ARGOCD_TOKEN = core.getInput('argocd-token');
 const VERSION = core.getInput('argocd-version');
 const ENV = core.getInput('environment');
-const PLAINTEXT = core.getInput('plaintext').toLowerCase() === "true";
+const PLAINTEXT = core.getInput('plaintext').toLowerCase() === 'true';
 let EXTRA_CLI_ARGS = core.getInput('argocd-extra-cli-args');
 if (PLAINTEXT) {
     EXTRA_CLI_ARGS += ' --plaintext';
@@ -1744,18 +1745,15 @@ function setupArgoCDCommand() {
 }
 function getApps() {
     return __awaiter(this, void 0, void 0, function* () {
-        let protocol = 'https';
-        if (PLAINTEXT) {
-            protocol = 'http';
-        }
-        const url = `${protocol}://${ARGOCD_SERVER_URL}/api/v1/applications`;
+        const url = `https://${ARGOCD_SERVER_URL}/api/v1/applications`;
         core.info(`Fetching apps from: ${url}`);
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         let responseJson;
         try {
             const response = yield node_fetch_1.default(url, {
                 method: 'GET',
-                headers: { Cookie: `argocd.token=${ARGOCD_TOKEN}` }
+                headers: { Cookie: `argocd.token=${ARGOCD_TOKEN}` },
+                agent: new https_1.default.Agent({ rejectUnauthorized: false })
             });
             responseJson = yield response.json();
         }
@@ -1835,7 +1833,7 @@ _Updated at ${new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angele
                 octokit.rest.issues.deleteComment({
                     owner,
                     repo,
-                    comment_id: comment.id,
+                    comment_id: comment.id
                 });
             }
         }
